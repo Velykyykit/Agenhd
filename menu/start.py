80ff36c8a11c8dcb2d5383fddb33f18e110f63fb
Velykyykit <81383936+Velykyykit@users.noreply.github.com>
2025-02-28 14:51:04 +0200

    Update start.py

--- menu/start.py
+++ menu/start.py
@@ -1,1 +1,16 @@
+import telebot
+from keyboards import phone_number_keyboard
+
+TOKEN = "your_telegram_bot_token"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "Вітаємо! Натисніть кнопку, щоб поділитися номером телефону:",
+        reply_markup=phone_number_keyboard()
+    )
+
+bot.polling(none_stop=True)
 
