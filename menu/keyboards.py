ccc9d6497cdd982020b4ea2b3bab66d9cfabac32
Velykyykit <81383936+Velykyykit@users.noreply.github.com>
2025-02-28 14:46:27 +0200

    Update keyboards.py

--- menu/keyboards.py
+++ menu/keyboards.py
@@ -1,1 +1,27 @@
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
+def get_phone_keyboard():
+    """
+    Клавіатура для запиту номера телефону.
+    """
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    phone_button = KeyboardButton("📲 Поділитися номером", request_contact=True)
+    restart_button = KeyboardButton("🔄 Почати спочатку")
+    markup.add(phone_button)
+    markup.add(restart_button)
+    return markup
+
+def get_restart_keyboard():
+    """
+    Клавіатура для повернення на початок.
+    """
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    restart_button = KeyboardButton("🔄 Почати спочатку")
+    markup.add(restart_button)
+    return markup
+
+def remove_keyboard():
+    """
+    Видаляє клавіатуру (наприклад, після авторизації).
+    """
+    return ReplyKeyboardRemove()
