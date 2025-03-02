bca75c688e24730811a84044257aba032bc5a76a
Velykyykit <81383936+Velykyykit@users.noreply.github.com>
2025-03-02 18:11:43 +0200

    Update sklad.py

--- data/sklad/sklad.py
+++ data/sklad/sklad.py
@@ -66,7 +66,7 @@ def show_all_stock(bot, message):
         pdf.cell(200, 10, f"Наявність товарів на складі (станом на {now})", ln=True, align="C")
         pdf.ln(10)
 
-        pdf.set_font("DejaVu", "", 10)
+        pdf.set_font("Helvetica", "", 10)
         pdf.cell(20, 8, "ID", border=1, align="C")
         pdf.cell(50, 8, "Курс", border=1, align="C")
         pdf.cell(50, 8, "Товар", border=1, align="C")
