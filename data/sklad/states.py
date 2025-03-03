48b9b6aeeb59eb56469993e7b857e6ca38d585e5
Velykyykit <81383936+Velykyykit@users.noreply.github.com>
2025-03-03 22:10:40 +0200

    Update states.py

--- data/sklad/states.py
+++ data/sklad/states.py
@@ -1,1 +1,7 @@
+from aiogram_dialog import StatesGroup, State
 
+class OrderSG(StatesGroup):
+    select_course = State()
+    select_item = State()
+    input_quantity = State()
+    confirm_order = State()
