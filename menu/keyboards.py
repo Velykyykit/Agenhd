189a6ba19be973268075235fe8985355ddea5559
Velykyykit <81383936+Velykyykit@users.noreply.github.com>
2025-03-03 15:20:35 +0200

    Update keyboards.py

--- menu/keyboards.py
+++ menu/keyboards.py
@@ -3,19 +3,18 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 async def get_phone_keyboard():
     """
     Клавіатура для запиту номера телефону.
-    Використовуємо лише кнопку для надання номера телефону.
     """
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    phone_button = KeyboardButton("📲 Поділитися номером", request_contact=True)
-    markup.add(phone_button)
-    return markup
+    return ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text="📲 Поділитися номером", request_contact=True)]],
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
 
 async def get_restart_keyboard():
     """
     Клавіатура для кнопки перезапуску.
-    Використовуємо лише кнопку для перезапуску.
     """
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    restart_button = KeyboardButton("🔄 Почати спочатку")
-    markup.add(restart_button)
-    return markup
+    return ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text="🔄 Почати спочатку")]],
+        resize_keyboard=True
+    )
